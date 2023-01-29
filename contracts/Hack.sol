fa2558fc1a91b93c9b1ff7c61ea1426cf162e4c7
Abhishek Sinha <avishkarabhishek786@gmail.com>
2023-01-29 12:04:33 +0530

    added reentrancy

--- contracts/Hack.sol
+++ contracts/Hack.sol
@@ -7,32 +7,39 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Hack is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Hack is ERC721, ERC721URIStorage, ERC721Burnable, Ownable, ReentrancyGuard {
     
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
     Counters.Counter private _mintByInstallmentCounter;
 
-    uint256 public MINT_TRACKER;
+    uint256 private MINT_TRACKER;
 
-    uint256 public immutable Max_Token;   // Max supply
+    uint256 private immutable Max_Token;   // Max supply
 
-    uint256 listPrice ;     //List price
+    uint256 private listPrice ;     //List price
 
-    bool public allowMint;  
+    bool private allowMint;  
 
-    uint public immutable freeLimit;
+    uint private immutable freeLimit;
 
-    bytes32 public merkleRoot ;  // root to be generated from a function            
+    uint private immutable freeLimitPeriod; // Max time period of free minting. Ex - 10 minutes, 3 days etc 
+
+    uint private immutable genesisTime; // time at which contract was deployed
+
+    bytes32 private merkleRoot ;  // root to be generated from a function            
 
     mapping(address => bool) public whitelistClaimed;
 
-    constructor(uint _freeLimit , uint _listPrice , uint _maxToken , uint _MINT_TRACKER) ERC721("Hack NFT", "HACK") {
+    constructor(uint _freeLimit , uint _listPrice , uint _maxToken , uint _MINT_TRACKER, uint _freeLimitPeriod) ERC721("Hack NFT", "HACK") {
         freeLimit = _freeLimit;
         listPrice = _listPrice;
         Max_Token = _maxToken;
         MINT_TRACKER = _MINT_TRACKER;
+        genesisTime = block.timestamp;
+        freeLimitPeriod = _freeLimitPeriod;
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -49,27 +56,31 @@ contract Hack is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         return _mintByInstallmentCounter.current();
     }
 
+    function getFreeLimit() public view returns(uint){
+        return freeLimit;
+    }
+
+    function getMerkleRoot() public view returns(bytes32){
+        return merkleRoot;
+    }
+
     // Function for updating listing price
 
-    function updateListPrice(uint256 _listPrice) public onlyOwner payable {
+    function updateListPrice(uint256 _listPrice) external onlyOwner payable {
         listPrice = _listPrice;
     }
 
     // Function for re-allow minting after the installment is over
 
-    function allowMinting(bool _allow) public onlyOwner{
+    function allowMinting(bool _allow) external onlyOwner{
         allowMint = _allow;
-    }
-
-    function getFreeLimit() public view returns(uint){
-        return freeLimit;
     }
 
     function setMerkleRoot(bytes32 _merkleRoot) external onlyOwner {
         merkleRoot = _merkleRoot;
     }
 
-    function freeMint(address to, string memory uri, bytes32[] calldata _merkleProof) public {
+    function freeMint(address to, string memory uri, bytes32[] calldata _merkleProof) public nonReentrant {
         require(!whitelistClaimed[msg.sender],"Address has already claimed.");
 
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
@@ -80,7 +91,6 @@ contract Hack is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         
         whitelistClaimed[msg.sender] = true;
 
-        //NFT_Mint(msg.sender);
         _mint(to, uri);
 
     }
@@ -96,20 +106,20 @@ contract Hack is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
             allowMint = false;
         }
 
-          _tokenIdCounter.increment();
+        _tokenIdCounter.increment();
         uint256 currentTokenId = _tokenIdCounter.current();
         
         require(currentTokenId<= Max_Token,"maximum limit reached");
 
-         _safeMint(to,currentTokenId);
-         _setTokenURI(currentTokenId, uri);
+        _safeMint(to,currentTokenId);
+        _setTokenURI(currentTokenId, uri);
 
     }
 
-    function safeMint(string memory uri) external payable returns(uint) {
+    function safeMint(string memory uri) external payable nonReentrant returns(uint) {
         
         uint256 currentTokenId = _tokenIdCounter.current();
-        require(currentTokenId >= freeLimit, "After free mints"); // This function can be only called after 1k link
+        require(block.timestamp < genesisTime + freeLimitPeriod || currentTokenId >= freeLimit, "After free mints"); // This function can be only called after 1k link
         
         require(msg.value >= listPrice , "Send enough ether to list");
 
